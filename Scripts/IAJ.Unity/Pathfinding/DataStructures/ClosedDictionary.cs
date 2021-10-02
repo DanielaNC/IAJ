2f91f974d9af6e1035c1967d16f8679048c22b5f
DanielaNC <71905088+DanielaNC@users.noreply.github.com>
2021-10-02 12:31:31 +0100

    Add files via upload

---
+++ Scripts/IAJ.Unity/Pathfinding/DataStructures/ClosedDictionary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding.DataStructures
+{
+    class ClosedDictionary : IClosedSet
+    {
+
+        private Dictionary<Vector2, NodeRecord> Closed { get; set; }
+
+        private NodeRecord auxRecord;
+        public ClosedDictionary()
+        {
+            this.Closed = new Dictionary<Vector2, NodeRecord>();
+            auxRecord = new NodeRecord(-1, -1);
+        }
+
+        public void Initialize()
+        {
+            this.Closed.Clear();
+            auxRecord = new NodeRecord(-1, -1);
+        }
+
+
+        public void AddToClosed(NodeRecord nodeRecord)
+        {
+           
+            var vec = new Vector2(nodeRecord.x, nodeRecord.y);
+            if (!Closed.TryGetValue(vec, out auxRecord))
+                Closed[vec] = nodeRecord;
+            else return;
+        }
+
+        public void RemoveFromClosed(NodeRecord nodeRecord)
+        {
+            this.Closed.Remove(new Vector2(nodeRecord.x, nodeRecord.y));
+        }
+
+        public NodeRecord SearchInClosed(NodeRecord nodeRecord)
+        {
+            //here I cannot use the == comparer because the nodeRecord will likely be a different computational object
+            //and therefore pointer comparison will not work, we need to use Equals
+            //LINQ with a lambda expression
+            var key = new Vector2(nodeRecord.x, nodeRecord.y);
+
+            if (!Closed.TryGetValue(key, out auxRecord))
+                return null;
+            else return auxRecord;
+        }
+
+      
+
+        public ICollection<NodeRecord> All()
+        {
+            return this.Closed.Values;
+        }
+
+      
+    }
+
+}
+
---
+++ Scripts/UI/UIManager.cs
@@ -0,0 +1,121 @@
+using Assets.Scripts.IAJ.Unity.Pathfinding;
+using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    //Pathfinding Manager reference
+    public PathfindingManager manager;
+
+    //Debug Components you can add your own here
+    Text debugCoordinates;
+    Text debugG;
+    Text debugF;
+    Text debugH;
+    Text debugWalkable;
+    Text debugtotalProcessedNodes;
+    Text debugtotalProcessingTime;
+    Text debugMaxNodes;
+    Text debugJPU;
+    Text debugJPD;
+    Text debugJPL;
+    Text debugJPR;
+    Text debugDArray;
+    Text debugBounds;
+
+    bool useGoal;
+    bool useJPS;
+
+    private int currentX, currentY;
+    VisualGridManager visualGrid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Simple way of getting the manager's reference
+        manager = GameObject.FindObjectOfType<PathfindingManager>();
+        visualGrid = GameObject.FindObjectOfType<VisualGridManager>();
+
+        // Retrieving the Debug Components
+        var debugTexts = this.transform.GetComponentsInChildren<Text>();
+        debugCoordinates = debugTexts[0];
+        debugH = debugTexts[1];
+        debugG = debugTexts[2];
+        debugF = debugTexts[3];
+        debugtotalProcessedNodes = debugTexts[4];
+        debugtotalProcessingTime = debugTexts[5];
+        debugMaxNodes = debugTexts[6];
+        debugWalkable = debugTexts[7];
+        debugDArray = debugTexts[8];
+        useGoal = manager.useGoalBound;
+        currentX = -1;
+        currentY = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // A Long way of printing useful information regarding the algorithm
+        var currentPosition = UtilsClass.GetMouseWorldPosition();
+        if (currentPosition != null)
+        {
+            int x, y;
+            if (manager.pathfinding.grid != null)
+            {
+                manager.pathfinding.grid.GetXY(currentPosition, out x, out y);
+                if (x == currentX & currentY == y || currentX == PathfindingManager.startingX && currentY == PathfindingManager.startingY
+                    || currentX == PathfindingManager.goalX && currentY == PathfindingManager.goalY)
+                    return;
+
+                currentX = x;
+                currentY = y;
+                if (x != -1 && y != -1)
+                {
+                    var node = manager.pathfinding.grid.GetGridObject(x, y);
+                    if (node != null)
+                    {
+                        debugCoordinates.text = " x:" + x + "; y:" + y;
+                        debugG.text = "G:" + node.gCost;
+                        debugF.text = "F:" + node.fCost;
+                        debugH.text = "H:" + node.hCost;
+                        debugWalkable.text = "IsWalkable:" + node.isWalkable;
+
+                        if (node.isWalkable)
+                        {
+                           if (useGoal)
+                            {
+                                var array = "";
+                                var goalBoundingPathfinder = (GoalBoundAStarPathfinding)manager.pathfinding;
+                                if (goalBoundingPathfinder.goalBounds.ContainsKey(new Vector2(x, y)))
+                                {
+                                    var boundingBox = goalBoundingPathfinder.goalBounds[new Vector2(x, y)];
+                                    array += "Left" + boundingBox["left"] + "\n";
+                                    array += "Right" + boundingBox["right"] + "\n";
+                                    array += "Up" + boundingBox["up"] + "\n";
+                                    array += "Down" + boundingBox["down"] + "\n";
+                                    debugDArray.text = array;
+                                    visualGrid.fillBoundingBox(node);
+
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+            }
+        }
+
+        if (this.manager.pathfinding.InProgress)
+        {
+                debugMaxNodes.text = "MaxNodes:" + manager.pathfinding.MaxOpenNodes;
+                debugtotalProcessedNodes.text = "TotalPNodes:" + manager.pathfinding.TotalProcessedNodes;
+                debugtotalProcessingTime.text = "TotalPTime:" + manager.pathfinding.TotalProcessingTime;
+            }
+        }
+}
